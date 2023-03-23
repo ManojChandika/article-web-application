be7e7bb731f5abeeb079f8a44877b899ae4b1fac
manoj <manojmpmc@gmail.com>
2023-03-23 08:42:06 +0530

    Initialize project

---
+++ src/main/java/com/example/articlewebapplication/ArticleWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.articlewebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/articlewebapplication/ArticleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.articlewebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
