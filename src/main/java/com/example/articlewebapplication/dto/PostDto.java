cce8cd6f55438d3d63c5e458b7eebb550e9e2c55
manoj <manojmpmc@gmail.com>
2023-03-24 10:47:01 +0530

    Handle the Exception Globally

--- src/main/java/com/example/articlewebapplication/dto/PostDto.java
+++ src/main/java/com/example/articlewebapplication/dto/PostDto.java
@@ -3,7 +3,6 @@ package com.example.articlewebapplication.dto;
 import lombok.Data;
 @Data
 public class PostDto {
-
         private Long id;
         private String title;
         private String description;
